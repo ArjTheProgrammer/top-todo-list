a4f7d1dcfd0ccaa516aa466a1094da4ceb2c8c0e
Jose Neil Silagan <neilsilagan@gmail.com>
2024-08-16 19:08:53 +0800

    Adjust the design

--- src/components/projectInput.js
+++ src/components/projectInput.js
@@ -6,14 +6,15 @@ export function projectInput(){
     const projectTitleInput = document.createElement("input");
     const projectTitle = document.createElement("span");
     const closeInput = document.createElement("button");
+    projectTitleInput.setAttribute("maxlength","20");
+    projectTitleInput.setAttribute("placeholder","Title (20 letters only)");
     closeInput.textContent = "X";
 
     projectTitleInput.addEventListener("keypress", (event) => {
         if (event.key === 'Enter'){
             projectTitle.textContent = `${projectTitleInput.value}`;
             projectContainer.append(projectTitle);
-            closeInput.remove();
-            projectTitleInput.remove();
+            projectInputCon.remove();
             projectButton.disabled = false;
         }
     });
